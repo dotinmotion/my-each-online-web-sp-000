55048ecb7a94e5de90b85630b7aeef8253d749bd
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-12 20:52:17 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,13 @@
 collection = [1, 2, 3, 4]
 
 def my_each(collection)
-  i=0
-  while i < collection.length do
-    yield(collection[i])
-  end
+ 
+ i=0
+ while i < collection.length 
+ yield(collection[i])
+ i+=1
+ end 
+ collection
 end
 
-my_each(collection) {|i| puts i}
+my_each(collection) {|x| puts x}
